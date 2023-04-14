23f1c74d8df0f8151d5281b0eb59583c1d9cab39
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-04-14 21:56:54 +0800

    simple dahs app

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-from flask import Flask, jsonify
-import os
+import dash
+from dash import html, dcc
+import plotly.express as px
+import pandas as pd
 
-app = Flask(__name__)
+# Load data
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly/datasets/master/iris.csv')
 
+# Create app
+app = dash.Dash(__name__)
 
-@app.route('/')
-def index():
-    return "HELLO THIS IS KREIOS, OVER."
+# Define layout
+app.layout = html.Div(children=[
+    html.H1(children='Iris Dataset'),
 
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
 
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': df.SepalLength, 'y': df.SepalWidth,
+                    'type': 'scatter', 'mode': 'markers'}
+            ],
+            'layout': {
+                'title': 'Sepal Length vs Sepal Width'
+            }
+        }
+    )
+])
+
+# Run app
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run_server(debug=True)
