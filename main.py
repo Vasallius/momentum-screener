20dbec572b1716d407149b9ecfa15e84f0f0f3bd
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-04-15 09:48:23 +0800

    sdfs

--- main.py
+++ main.py
@@ -1,37 +1,10 @@
-import dash
-from dash import html, dcc
-import plotly.express as px
-import pandas as pd
+from dash import Dash, html
 
-# Load data
-df = pd.read_csv(
-    'https://raw.githubusercontent.com/plotly/datasets/master/iris.csv')
+app = Dash(__name__)
 
-# Create app
-app = dash.Dash(__name__)
-
-# Define layout
-app.layout = html.Div(children=[
-    html.H1(children='Iris Dataset'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': df.SepalLength, 'y': df.SepalWidth,
-                    'type': 'scatter', 'mode': 'markers'}
-            ],
-            'layout': {
-                'title': 'Sepal Length vs Sepal Width'
-            }
-        }
-    )
+app.layout = html.Div([
+    html.Div(children='Hello World')
 ])
 
-# Run app
 if __name__ == '__main__':
     app.run_server(debug=True)
