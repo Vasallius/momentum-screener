858fe267291d33481f9c000151b954d7e7a5d55d
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-04-18 22:40:51 +0800

    working script

---
+++ assets/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                custom: '#0083FF',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
